088b586c5c62e94d82b2e4955fb3475de0da453a
fws-stefan <stefan.stosic@forwardslashny.com>
2024-01-18 15:31:01 +0100

    made the code more pythonic

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # secure-insecure-urls
-A simple script that checks your file with bunch of URLs and sort out which are secure and which are not secure. 
+
+A simple script that checks your file with bunch of URLs and sort out which are secure and which are not secure.
+
+## Usage
+
+Run the script using command `./secure_insecure_urls.py` or `python3 secure_insecure_urls.py` and follow the instructions
--- secure_insecure_urls.py
+++ secure_insecure_urls.py
@@ -1,73 +1,77 @@
-###sorting function
-def checkthistxt (path,path1):
-    ##open text file with urls
-    url_file=open(path,'r')
-    opened=url_file.read()
-    print('''The file that contain urls in it looks like this:\n\n'''+opened+'\n')
-    ##regex for http and https
-    httporsreg=re.compile(r'''((http://|https://) #in urls there http:// or https://
-([a-zA-Z0-9]*\.)? #then there is something.something.something
-([a-zA-Z0-9]*\.)? #and mby some more
-([a-zA-Z0-9/]{2,})?) #usualy url is ended something like this, 2 or more char
-''',re.VERBOSE)
-    sites=httporsreg.findall(opened) ##this is a container for all found urls
-    ##counters
-    j=1
-    k=1
-    ##for loop goes through urls container
-    for i in range (len(sites)):
-        if sites[i][1]=='https://':#check first group of found urls
-            for_print=sites[i][0]#container for https urls
-            Note=open(path1+r'/secure_sites.txt','a')
-            Note.write(str(j)+'- '+for_print+'\n')
-            j=j+1
-            Note.close()
-        ##same story here
-        elif sites[i][1]=='http://':
-            for_print=sites[i][0]
-            ##this is just one time variable like the one above, the point is to write in another file
-            Note=open(path1+r'/not_secure_sites.txt','a')
-            Note.write(str(k)+'- '+for_print+'\n')
-            k=k+1
-            Note.close()
+#!/usr/bin/env python3
+
+import os
+import re
+
+def check_this_txt(path, path1):
+    # Open text file with URLs
+    with open(path, 'r') as url_file:
+        opened = url_file.read()
+        print('The file that contains URLs looks like this:\n\n' + opened + '\n')
+
+    # Regex for http and https
+    http_https_regex = re.compile(r'''((http://|https://)
+                            ([a-zA-Z0-9]*\.)?
+                            ([a-zA-Z0-9]*\.)?
+                            ([a-zA-Z0-9/]{2,})?)''', re.VERBOSE)
+    
+    # Container for all found URLs
+    sites = http_https_regex.findall(opened)
+
+    # Counters
+    j = 1
+    k = 1
+
+    # For loop goes through URLs container
+    for i in range(len(sites)):
+        if sites[i][1] == 'https://':
+            # Check the first group of found URLs
+            for_print = sites[i][0]
+            note = open(os.path.join(path1, 'secure_sites.txt'), 'a')
+            note.write(str(j) + '- ' + for_print + '\n')
+            j += 1
+            note.close()
+        elif sites[i][1] == 'http://':
+            # Same story here
+            for_print = sites[i][0]
+            note = open(os.path.join(path1, 'not_secure_sites.txt'), 'a')
+            note.write(str(k) + '- ' + for_print + '\n')
+            k += 1
+            note.close()
+
     print('It is done...')
     print('Secure web sites are:\n')
-    sec=open(path1+r'/secure_sites.txt','r')
-    sresult=sec.read()
-    print(sresult)
-    sec.close()
+    with open(os.path.join(path1, 'secure_sites.txt'), 'r') as sec:
+        secure_results = sec.read()
+        print(secure_results)
+
     print('Not secure web sites are:\n')
-    nsec=open(path1+r'/not_secure_sites.txt','r')
-    nresult=nsec.read()
-    print(nresult)
-    nsec.close()
+    with open(os.path.join(path1, 'not_secure_sites.txt'), 'r') as nsec:
+        not_secure_results = nsec.read()
+        print(not_secure_results)
+
     print('Files with Secure and Not secure web sites are created')
 
-##function for deleting files
 def no_lose_ends(path1):
-    os.remove(path1+r'/secure_sites.txt')
-    os.remove(path1+r'/not_secure_sites.txt')
+    os.remove(os.path.join(path1, 'secure_sites.txt'))
+    os.remove(os.path.join(path1, 'not_secure_sites.txt'))
 
-###MAIN
-import os, re
-path_url=input('''Insert path to text file (file included) with all urls
-which you want to sort out as secure and not secure
-(for example /home/yourname/Desktop/url.txt)
-''')
-path_save=input('''Insert path to file in which
-you want to save secure and not secure urls
-(for example /home/yourname/Desktop)
-''')
-checkthistxt(path_url,path_save)
+# MAIN
+path_url = input('''Insert the path to a text file with all URLs that you want to sort out as secure and not secure
+(for example /home/username/Desktop/url.txt)\n''')
+path_save = input('''Insert the path to a directory in which you want to save sorted, secure and not secure URLs
+(for example /home/username/Desktop)\n''')
+
+check_this_txt(path_url, path_save)
+
 while True:
-    answer=input('''Do you want this files with secure and not secure urls to stay on your computer?
-Y for Yes, N for No)\n''')
-    if answer!='Y' and answer!='N':
+    answer = input('''Do you want these files with secure and not secure URLs to stay on your computer?
+Y for Yes, N for No\n''')
+    if answer not in ('Y', 'N'):
         print('Wrong input! (Y for Yes, N for No)')
         continue
-    elif answer=='Y':
+    elif answer == 'Y':
         break
-    elif answer=='N':
+    elif answer == 'N':
         no_lose_ends(path_save)
-    break
-
+        break
